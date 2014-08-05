b21ec73916e141076b7a5b4b59bc3844303fb99b
Rifatul Islam Shovon <rifatul.islam@therapservices.net>
2014-08-05 12:46:33 +0600

    profile and javascript bug fixed

--- application/book-worm/src/main/java/net/therap/controller/ProfileController.java
+++ application/book-worm/src/main/java/net/therap/controller/ProfileController.java
@@ -2,19 +2,20 @@ package net.therap.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author shakhawat.hossain
+ * @author rifatul.islam
  * @since 8/5/14 10:56 AM
  */
 
 @Controller
 public class ProfileController {
-
-    @RequestMapping(value = "/profile", method = RequestMethod.GET)
-    public String getProfilePage(ModelMap modelMap) {
+    @RequestMapping(value = "/profile/{userId}", method = RequestMethod.GET)
+    public String getProfilePage(@PathVariable int userId, ModelMap modelMap) {
         return "user/profile";
     }
 }
